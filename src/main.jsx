36d79885b356f430d41a33c9cfd457a206ec1b5c
Divya-126 <dhotedivya126@gmail.com>
2026-07-26 17:47:24 +0530

    Fix CSS imports and typography

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import "./styles/global.css";
 import "./index.css";
 
 import App from "./App.jsx";
-
 import { Toaster } from "react-hot-toast";
-
 import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
@@ -16,7 +13,6 @@ createRoot(document.getElementById("root")).render(
     <ThemeProvider>
       <BrowserRouter>
         <App />
-
         <Toaster
           position="top-right"
           toastOptions={{
